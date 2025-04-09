5ee14040387bf354a8327e83e84ad5fb80d013a2
unknown <asadalgalib9@gmail.com>
2025-04-09 18:44:09 +0600

    add user

--- index.js
+++ index.js
@@ -8,16 +8,56 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: true
-  }));
-  app.use(cookieParser());
-  app.use(express.json());
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
+app.use(cookieParser());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bidfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const database = client.db('GreenPulse');
+    const userCollection = database.collection('Users');
+
+    // add user
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.status(200).json({
+          message: 'Login successful',
+        });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+  }
+  finally {
+
+  }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Talkify server is Okkkkkay');
-  })
-  
-  app.listen(port, (req, res) => {
-    console.log(`server is running on ${port}`);
-  })
+  res.send('Talkify server is Okkkkkay');
+})
+
+app.listen(port, (req, res) => {
+  console.log(`server is running on ${port}`);
+})
